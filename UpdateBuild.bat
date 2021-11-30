5ffa11556bb67100385a2a42e20c17e42c3c2104
Ji-Rath <garethaguilar@gmail.com>
2021-11-30 12:20:33 -0600

    Begin debugging bugs during imperfect network conditions

--- UpdateBuild.bat
+++ UpdateBuild.bat
@@ -1,3 +1,5 @@
+@echo off
+echo Update windows package for SquibGame on itchio?
 PAUSE
 butler push D:\Users\garet\Documents\Git\UEProjects\SquibGame\Build\WindowsNoEditor jirath/squibgame:windows
 PAUSE
